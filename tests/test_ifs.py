4f0b964ff8d9b9fb59da50af1b29ee92161f7e0b
Sou-Cheng Choi <schoi32@iit.edu>
2022-12-10 16:29:18 -0600

    Fix tests for IFS

--- tests/test_ifs.py
+++ tests/test_ifs.py
@@ -5,11 +5,11 @@ from imfdatapy.imf import *
 class MyTestCase(unittest.TestCase):
 
     def test_imf_oo(self):
-        ifs = IMF()
+        ifs = IFS()
         self.assertEqual(ifs.start_date, None)
 
     def test_imf_ifs_eg1(self):
-        ifs = IMF(search_terms=["gross domestic product, real"], countries=["US"], period='Q', start_date="2000",
+        ifs = IFS(search_terms=["gross domestic product, real"], countries=["US"], period='Q', start_date="2000",
                   end_date="2022")
         df = ifs.download_data()
         self.assertGreaterEqual(df.shape[0], 174)
@@ -20,7 +20,7 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_imf_ifs_eg2(self):
-        ifs = IMF(search_terms=["gross Domestic Product, Real"], countries=["CA", "RU"],
+        ifs = IFS(search_terms=["gross Domestic Product, Real"], countries=["CA", "RU"],
                   period='Q', start_date="1970", end_date="2022")
         df = ifs.download_data()
         self.assertGreaterEqual(df.shape[0], 361)
@@ -29,7 +29,7 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_imf_ifs_eg3(self):
-        ifs = IMF(countries=["US"], period='Q', start_date="2000", end_date="2022")
+        ifs = IFS(countries=["US"], period='Q', start_date="2000", end_date="2022")
         df = ifs.download_data()
         self.assertGreaterEqual(df.shape[0], 174)
         meta_df = ifs.get_meta()
