d3f3591b76c06021ffe4f59aa8b1f013b584f40f
Sou-Cheng Choi <schoi32@iit.edu>
2023-01-28 13:15:13 -0600

    Enhance logger

--- src/imfdatapy/imf_log.py
+++ src/imfdatapy/imf_log.py
@@ -4,33 +4,39 @@ from datetime import datetime
 import os
 
 
-time_stamp = str(datetime.now())[:16].replace(" ", "-").replace(":", "-")
+time_stamp = str(datetime.now())[:19].replace(" ", "-").replace(":", "-")
 cwd = os.getcwd()
+is_file_logger = True
 if (cwd[-3:]=="src") or (cwd[-4:]=="demo") or (cwd[-5:]=="tests"):
     logdir = "../log"
 elif cwd[-9:] == "imfdatapy":
     logdir = "../../log"
 else:
-    print("WARN: Contact the IM")
+    print("WARN: Just creating a logger with output to screen.")
+    is_file_logger = False
 
-if not os.path.exists(logdir):
-    print(f"WARN: Creating log directory {logdir}")
-    os.mkdir(logdir)
-log_file = f"{logdir}/imfdatapy_{time_stamp}.log"
+if is_file_logger:
+    if not os.path.exists(logdir):
+        print(f"WARN: Creating log directory {logdir}")
+        os.mkdir(logdir)
+    log_file = f"{logdir}/imfdatapy_{time_stamp}.log"
+
 log_format = "%(asctime)s %(filename)s:%(lineno)d - %(levelname)s - %(message)s"
 
 rootLogger = logging.getLogger()
 
-file_handler = logging.FileHandler(log_file)
-file_handler.setFormatter(logging.Formatter(log_format))
+if is_file_logger:
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(logging.Formatter(log_format))
 
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logging.Formatter(log_format))
 rootLogger.addHandler(consoleHandler)
 
 logger = logging.getLogger("imfdatapy_log")
-
-logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
-logger.info(f"Current directory {os.getcwd()}")
-logger.info(f"Started log {log_file}")
+
+if is_file_logger:
+    logger.addHandler(file_handler)
+    logger.info(f"Current directory {os.getcwd()}")
+    logger.info(f"Started log {log_file}")
